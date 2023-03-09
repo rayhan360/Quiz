64fe16028257fe6a7b360513557ad955efd3cc41
Md: Redoan Ahmmad <hossainrayhan897@gmail.com>
2023-03-09 21:21:58 +0600

    Project done

---
+++ quiz.js
@@ -0,0 +1,118 @@
+let quiz_rules = [
+    "You have only 20 seconds to for each question.",
+    "Once You select any answer. It can't be undone.",
+    "You'll get points on the basic of your correct answer."
+];
+
+
+let quiz_questions = [
+    // Question 1
+    {
+        // Question
+        question: "What does HTML stand for?",
+
+        //options
+        options : [
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language",
+            "Home Tool Markup Language",
+            "Hyper Text Making Language",
+        ],
+
+        //correct answer
+        answer : 1,
+    },
+    {
+        // Question
+        question: "What does CSS stand for?",
+
+        //options
+        options : [
+            "Computer Style Sheet",
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Creative Style Sheets",
+        ],
+
+        //correct answer
+        answer : 2,
+    },
+    {
+        // write yout question here
+        question: "Inside which HTML element do we put the JavaScript?",
+    
+        options: [
+            "script",
+            "scripting",
+            "js",
+            "javascript",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What does SQL stand for?",
+    
+        options: [
+            "Structure Query Language",
+            "Strong Question Language",
+            "Structured Question Language",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What does PHP stand for?",
+    
+        options: [
+            "PHP: Hypertext Preprocessor",
+            "Personal Hypertext Processor",
+            "Private Homepage",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Which of the following is correct?",
+    
+        options: [
+            "JQuery is a JavaScript Library",
+            "JQuery is a JSON Library",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Bootstrap 3 is mobile-first.",
+    
+        options: [
+            "False",
+            "True",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What is a correct syntax to output 'Hello World' in C++?",
+    
+        options: [
+            'System.out.printLn("Hello World")',
+            'cout << "Hello World"',
+            'print("Hello World")',
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+
+    ];
